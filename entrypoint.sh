9994120293763e95d190cf8639dc82e4b521df7c
Sergey Seperovich <sseper@sergeyseperovich-cy913vqx0r.tail1bb4d5.ts.net>
2025-09-19 18:27:51 +0200

    grub include shim: set prefix to ()/boot/grub so ISO modules can be loaded when including ISO grub.cfg

--- entrypoint.sh
+++ entrypoint.sh
@@ -912,6 +912,9 @@ unset net_default_server
 # Point to the installer ISO under the version path
 loopback loop0 (http,${SERVER_IP})/${version}/installer.iso
 set root=loop0
+# Ensure GRUB can load modules from the ISO
+set prefix=($root)/boot/grub
+export prefix
 configfile ($root)/EFI/BOOT/grub.cfg
 EOF
         chmod 644 "/data/httpboot/${version}/EFI/BOOT/grub_include.cfg"
